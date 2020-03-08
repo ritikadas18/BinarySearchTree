f1f590873e624263f4f2148f2b1388ecba8b625a
ritikadas18 <ritika.das_cs18@gla.ac.in>
2020-03-08 15:12:19 +0530

    insert call 1

--- begin/src/main/myMain.java
+++ begin/src/main/myMain.java
@@ -3,6 +3,6 @@ import BinarySearchTree.myBinarySearchTree;
 public class myMain {
     public static void main(String[] args) {
         myBinarySearchTree bst = new myBinarySearchTree();
-
+        bst.insert(40);
     }
 }
