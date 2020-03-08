956fde0edd56267a469a3b78898f8211d9e8da84
ritikadas18 <ritika.das_cs18@gla.ac.in>
2020-03-08 12:55:54 +0530

    variables declared and constructor made

--- begin/src/node/TreeNode.java
+++ begin/src/node/TreeNode.java
@@ -1,4 +1,13 @@
 package node;
 
 public class TreeNode {
+    private int data;
+    private TreeNode left;
+    private TreeNode right;
+
+    public TreeNode(int data) {
+        this.data=data;
+        this.left=left;
+        this.right=right;
+    }
 }
