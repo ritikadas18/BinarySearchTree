ac852078c3f84e604f1de187efd6fcb18d4bd451
ritikadas18 <ritika.das_cs18@gla.ac.in>
2020-03-08 15:05:07 +0530

    traverse type 3 method added:traversePostOrder

--- begin/src/BinarySearchTree/myBinarySearchTree.java
+++ begin/src/BinarySearchTree/myBinarySearchTree.java
@@ -69,6 +69,10 @@ public class myBinarySearchTree {
             traverseInOrder(node.getRight());
         }
     }
+    public void traversePostOrder(TreeNode node)
+    {
+        //statements
+    }
 
 
     }
