34bd13f46ad0661f9630badccc0bd5f116c40096
ritikadas18 <ritika.das_cs18@gla.ac.in>
2020-03-08 15:11:04 +0530

    object created

--- begin/src/main/myMain.java
+++ begin/src/main/myMain.java
@@ -2,6 +2,7 @@ package main;
 import BinarySearchTree.myBinarySearchTree;
 public class myMain {
     public static void main(String[] args) {
+        myBinarySearchTree bst = new myBinarySearchTree();
 
     }
 }
