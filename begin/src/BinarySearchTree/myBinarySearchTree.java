ba6bde85e040937e28f4d3643bcf6e8e2e097056
ritikadas18 <ritika.das_cs18@gla.ac.in>
2020-03-08 14:05:43 +0530

    insert method statements completed

--- begin/src/BinarySearchTree/myBinarySearchTree.java
+++ begin/src/BinarySearchTree/myBinarySearchTree.java
@@ -8,6 +8,27 @@ public class myBinarySearchTree {
     //insert
     public void insert(int data) {
         //statements
+        TreeNode node = new TreeNode(data);
+        if (root == null) {
+            root = node;
+        } else {
+            TreeNode temp = root;
+            TreeNode parent = null;
+            while (temp != null) {
+                parent=temp;
+                if (node.getData() <= temp.getData()) {
+                    temp = temp.getLeft();
+                }
+                else {
+                    temp = temp.getRight();
+                }
+                if (node.getData() <= parent.getData()) {
+                    parent.setLeft(node);
+                }
+                else {
+                    parent.setRight(node);
+                }
+            }
+        }
     }
-
 }
